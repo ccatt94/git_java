8c49b1b915a32b008f1a2dae6c0e28321940934b
WIN-G8EHF2IUQB8\ICT02_19 <sally940828@naver.com>
2024-07-10 18:12:44 +0900

    7월 10일 학습

---
+++ java_hello/src/SysOutExample.java
@@ -0,0 +1,19 @@
+/*
+ 1. 주석을 주면 해당 부분을 컴파일 하지 않음()
+ 2. 주석의 목적 - 소스코드 설명
+ */
+
+public class SysOutExample {
+
+	public static void main(String[] args) {
+		System.out.println(7);// 정수
+
+		System.out.println(3.15);// 실수
+		System.out.println(3 + 5);
+
+		System.out.println("3 + 5");
+		System.out.println("3 + 5 = " + 8);
+		System.out.println("3 + 5 = " + "8");// 문자열
+	}
+
+}
