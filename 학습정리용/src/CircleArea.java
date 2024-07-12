3a1cecdc91dc99a6a552e4a111fb4bb9f96c231e
WIN-G8EHF2IUQB8\ICT02_19 <sally940828@naver.com>
2024-07-12 09:23:33 +0900

    7월 11일 학습 정리

---
+++ 학습정리용/src/CircleArea.java
@@ -0,0 +1,13 @@
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+		double radius = 10;
+		final double PI = 3.14;
+		
+		double area = radius * radius * PI;
+		System.out.println("반지름:" + radius + ' ' + "넓이:" + area);
+		System.out.println("넓이가 100 미만 입니까?" + "\n" + (100 > area));
+	}
+
+}
---
+++ 학습정리용/src/TriangleArea.java
@@ -0,0 +1,14 @@
+
+public class TriangleArea {
+
+	public static void main(String[] args) {
+
+		double width = 10;
+		double height = 10;
+		
+		double area = width * height / 2;
+	
+		System.out.println("20보다 큽니까?\n" + (20 < area));
+	}
+
+}
