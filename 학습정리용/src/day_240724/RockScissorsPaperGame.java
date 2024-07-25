09c41d0038067a75c3be9deb2b2eefb1e351f022
ccatt94 <sally940828@naver.com>
2024-07-25 09:16:52 +0900

    Update RockScissorsPaperGame.java
    수정

--- 학습정리용/src/day_240724/RockScissorsPaperGame.java
+++ 학습정리용/src/day_240724/RockScissorsPaperGame.java
@@ -10,9 +10,34 @@ public class RockScissorsPaperGame {
 		System.out.println("가위, 바위, 보 중 하나를 입력하세요.");
 		
 		Scanner sc = new Scanner(System.in);
-		String position = sc.nextLine();
 		
-		int num = (int)(Math.random() * 3 + 1);
+		int com = (int)(Math.random() * 3 + 1);
+                String user = sc.nextLine();
+                Switch (com) {
+case 1 :
+System.out.println("가위");
+break;
+case 2 :
+System.out.println("바위");
+break;
+case 3 :
+System.out.println("보");
+break;
+}
+
+Switch (user) {
+case 1 :
+System.out.println("가위");
+break;
+case 2 :
+System.out.println("바위");
+break;
+case 3 :
+System.out.println("보");
+break;
+}
+
+if(
 		
 //		if(position = "가위" ) {
 			
